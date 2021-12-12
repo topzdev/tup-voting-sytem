dce745711b23ba3a9b76cb1177592957145df3fb
topzdev <christianlugod05@gmail.com>
2021-12-12 20:48:18 +0800

    database port added

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -13,7 +13,7 @@ console.log(join(__dirname, ormPath, "entity", "*.entity.{js,ts}"));
 module.exports = {
   type: process.env.DATABASE_TYPE,
   host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.PORT),
+  port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
