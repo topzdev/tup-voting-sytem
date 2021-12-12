65509bd9dddc4e8567657639422ce414820d7238
topzdev <christianlugod05@gmail.com>
2021-12-12 20:49:19 +0800

    database port edited

--- server/src/configs/index.ts
+++ server/src/configs/index.ts
@@ -19,7 +19,7 @@ const configs = {
   database: {
     type: process.env.DATABASE_TYPE,
     host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.PORT),
+    port: parseInt(process.env.DATABASE_PORT),
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
