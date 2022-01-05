37b719ae1121def2d333713af3afa2e7d9f3794d
errol <capistranoerrol05@gmail.com>
2022-01-05 15:41:40 +0800

    ...

--- server/src/modules/party/party.validator.ts
+++ server/src/modules/party/party.validator.ts
@@ -1,20 +1,8 @@
 import { body } from "express-validator";
 
 // docs: https://express-validator.github.io/docs/index.html
-
 const validations = {
   id: body("id").notEmpty().withMessage("ID is required"),
-  // slug: body("slug")
-  //   .notEmpty()
-  //   .withMessage("Slug is required")
-  //   .not()
-  //   .contains(" ")
-  //   .withMessage("Slug must not contain space")
-  //   .isLowercase()
-  //   .withMessage("Slug must be lowercase")
-  //   .not()
-  //   .matches(/[ `!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~\.\*\\\t\n\r]+/)
-  //   .withMessage("Slug must not contain number and special characters"),
   title: body("title").notEmpty().withMessage("Title is required"),
   election_id: body("election_id")
     .notEmpty()
