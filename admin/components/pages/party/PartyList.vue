51fb39df1a41bb35ccfaf0603d84e24ccbd68244
Harold Tungala <tungalaharold@gmail.com>
2022-01-08 21:01:15 +0800

    party done but UI problem with cover page needhelp

---
+++ admin/components/pages/party/PartyList.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <v-row no-gutters class="my-4">
+      <v-container >
+        <v-avatar size="100" >
+            <img src="https://picsum.photos/seed/picsum/200/300"/>
+        </v-avatar>
+        <h1 class="headline-6 front-weight-medium my-4">Organization Party</h1>
+      </v-container>
+      <template v-if="items.length">
+        <v-col v-for="item in items" :key="item.id" cols="12" class="mb-3">
+          <party-card :data="item" />
+        </v-col>
+      </template>
+      <v-col v-else class="text-center"> No item found </v-col>
+    </v-row>
+  </span>
+</template>
+
+<script>
+import Vue, { PropOptions } from "vue";
+import PartyCard from "./cards/PartyCard.vue";
+
+export default Vue.extend({
+  components: {
+    PartyCard,
+  },
+
+  props: {
+    items: Array,
+    totalCount: {
+      default: 0,
+      types: Number,
+    },
+  },
+});
+</script>
+
+<style>
+</style>
