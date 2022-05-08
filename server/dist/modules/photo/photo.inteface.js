8fa53380843cdd3a808c5a518db4839394b162fa
topzdev <christianlugod05@gmail.com>
2022-05-08 18:17:16 +0800

    typescript and deployment fix

--- server/dist/modules/photo/photo.inteface.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=photo.inteface.js.map
---
+++ server/src/migration/1652000935041-Deployment582022.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Deployment5820221652000935041 implements MigrationInterface {
+    name = 'Deployment5820221652000935041'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE SEQUENCE IF NOT EXISTS "election_voted_id_seq" OWNED BY "election_voted"."id"`);
+        await queryRunner.query(`ALTER TABLE "election_voted" ALTER COLUMN "id" SET DEFAULT nextval('"election_voted_id_seq"')`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "election_voted" ALTER COLUMN "id" DROP DEFAULT`);
+        await queryRunner.query(`DROP SEQUENCE "election_voted_id_seq"`);
+    }
+
+}
