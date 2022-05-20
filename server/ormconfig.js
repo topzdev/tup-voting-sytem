6612f742c2569fd376c739d7e2bc8983ddb1cffa
topzdev <christianlugod05@gmail.com>
2022-05-20 23:05:59 +0800

    ormconfig updated

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -5,11 +5,10 @@ const SnakeNamingStrategy =
 
 var __prod__ = process.env.NODE_ENV === "production";
 
-require("dotenv").config(
-  !__prod__
-    ? { path: path.resolve(process.cwd(), "development.env") }
-    : undefined
-);
+require("dotenv").config();
+// !__prod__
+//   ? { path: path.resolve(process.cwd(), "development.env") }
+//   : undefined
 
 var ormPath = !__prod__ ? "src" : "dist";
 console.log(join(__dirname, ormPath, "entity", "*.entity.{js,ts}"));
@@ -30,11 +29,13 @@ module.exports = {
   ],
   migrations: [join(__dirname, ormPath, "migration", "**", "*.{js,ts}")],
   subscribers: [join(__dirname, ormPath, "subscriber", "**", "*.{js,ts}")],
-  ssl: false,
-  // ssl: {
-  //   require: true,
-  //   rejectUnauthorized: false,
-  // },
+  // ssl: false,
+  ssl: Boolean(process.env.DATABASE_SSL)
+    ? {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    : false,
   cli: {
     entitiesDir: join(__dirname, ormPath, "entity"),
     migrationsDir: join(__dirname, ormPath, "migration"),
