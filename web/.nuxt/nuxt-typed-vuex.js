214d70855f90986472f71f74d5a20dbdc76241f1
topzdev <christianlugod05@gmail.com>
2022-05-29 16:24:25 +0800

    Post .gitignore changes

--- web/.nuxt/nuxt-typed-vuex.js
+++
@@ -1,9 +0,0 @@
-import { getAccessorFromStore } from 'typed-vuex'
-
-import { createStore } from 'C:/development/tup-voting-system/web/.nuxt/store'
-
-const storeAccessor = getAccessorFromStore(createStore())
-
-export default async ({ store }, inject) => {
-  inject('accessor', storeAccessor(store))
-}
